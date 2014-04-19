a8e35c7bfd887d895c69ad46bcd19c8998a93e87
linuxsoares <linux.soares@gmail.com>
2014-04-19 14:47:38 -0300

    Adicionando Filmes ao projeto.

--- app/controllers/FilmeCRUD.java
+++ app/controllers/FilmeCRUD.java
@@ -1,12 +1,20 @@
 package controllers;
 
+import static play.data.Form.form;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 import models.Diretor;
 import models.Filme;
-import play.api.mvc.Result;
-import play.mvc.Controller;
+import play.Play;
 import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
 
 /**
  * Created by gilmar on 18/04/14.
@@ -46,4 +54,71 @@ public class FilmeCRUD extends Controller{
         return lista();
     }
 
+    public static Result novoFilme() {
+
+        List<Diretor> diretores = Diretor.find.findList();
+
+        return ok(views.html.novoFilme.render(filmeForm,diretores));
+
+    }
+
+    public static Result detalhar(Long id) {
+        Form<Filme> filmeForm = form(Filme.class).fill(Filme.find.byId(id));
+        return ok(views.html.alterarFilme.render(id,filmeForm));
+    }
+
+    public static Result imagem(Long id) throws IOException {
+
+        String diretorioDeImagens = Play.application().configuration().getString("diretorioDeImagens");
+
+        File imagem = new File(diretorioDeImagens,id + ".png");
+
+        return ok(new FileInputStream(imagem));
+
+    }
+
+    public static Result upload() {
+
+        MultipartFormData body = request().body().asMultipartFormData();
+        FilePart picture = body.getFile("picture");
+        String extensaoPadraoDeImagens = Play.application().configuration().getString("extensaoPadraoDeImagens");
+        if (picture != null) {
+
+            String filmeId = form().bindFromRequest().get("filmeId");
+            String imagem = filmeId + extensaoPadraoDeImagens;
+            String contentType = picture.getContentType();
+            File file = picture.getFile();
+
+            String diretorioDeImagens = Play.application().configuration().getString("diretorioDeImagens");
+            String contentTypePadraoDeImagens = Play.application().configuration().getString("contentTypePadraoDeImagens");
+
+            if (contentType.equals(contentTypePadraoDeImagens)) {
+
+                file.renameTo(new File(diretorioDeImagens,imagem));
+                return ok(views.html.upload.render("Arquivo  \"" + imagem + "\" do tipo [" + contentType + "] foi carregado com sucesso !"));
+
+            } else {
+
+                return ok(views.html.upload.render("Imagens apenas no formato \"" + contentTypePadraoDeImagens + "\" serão aceitas!"));
+
+            }
+
+        } else {
+            flash("error","Erro ao fazer upload");
+            return redirect(routes.Application.index());
+        }
+    }
+
+    public static Result alterar(Long id) {
+
+        form(Filme.class).fill(Filme.find.byId(id));
+
+        Form<Filme> alterarForm = form(Filme.class).bindFromRequest();
+        if (alterarForm.hasErrors()) { return badRequest(views.html.alterarFilme.render(id,alterarForm)); }
+        alterarForm.get().update(id);
+        flash("sucesso","Filme " + alterarForm.get().nome + " alterado com sucesso");
+
+        return redirect(routes.FilmeCRUD.lista());
+
+    }
 }
--- app/models/Diretor.java
+++ app/models/Diretor.java
@@ -1,25 +1,38 @@
 package models;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import play.db.ebean.Model;
 import play.data.validation.Constraints;
+import play.db.ebean.Model;
 
 @Entity
 public class Diretor extends Model {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	public Long id;
-	
-	@Constraints.Required
-	public String nome;
-	
-	public static Model.Finder<Long,Diretor> find = new Model.Finder<Long,Diretor>(Long.class,Diretor.class);
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    public Long id;
+
+    @Constraints.Required
+    public String nome;
+
+    public static Model.Finder<Long,Diretor> find = new Model.Finder<Long,Diretor>(Long.class,Diretor.class);
+
+    public static Map<String,String> options() {
+        LinkedHashMap<String,String> options = new LinkedHashMap<String,String>();
+        for (Diretor c : Diretor.find.orderBy("nome").findList()) {
+            options.put(c.id.toString(),c.nome);
+        }
+        return options;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
 
 }
